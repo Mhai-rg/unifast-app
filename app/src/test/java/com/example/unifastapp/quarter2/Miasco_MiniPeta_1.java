b14be1589e7639570f862d47d08303374af24e69
miascosheymia <miamhayie@gmail.com>
2026-09-11 22:14:34 +0800

    Miasco_MiniPeta_1 code

---
+++ app/src/test/java/com/example/unifastapp/quarter2/Miasco_MiniPeta_1.java
@@ -0,0 +1,20 @@
+package com.example.unifastapp.quarter2;
+
+import org.junit.Test;
+
+public class Miasco_MiniPeta_1 {
+    @Test
+    public void printMyProfile() {
+
+        String myName = "Miasco";
+        String petName = "Hank";
+        String favFood = "Adobo";
+        int myAge = 16;
+
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myName + " and i am " + myAge + " years old.");
+        System.out.println("I have a wonderful pet named " + petName + ".");
+        System.out.println("if I could, I would eat " + favFood + " every single day!");
+    }
+}
+
