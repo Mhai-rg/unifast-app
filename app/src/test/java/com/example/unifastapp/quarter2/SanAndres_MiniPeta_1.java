7f14a7754226eff99358fbebedef1732f9fe1cbd
Steve Francis San Andres <sanandresstevefrancis@gmail.com>
2026-09-11 23:34:22 +0800

    SanAndres_MiniPeta_1 code

---
+++ app/src/test/java/com/example/unifastapp/quarter2/SanAndres_MiniPeta_1.java
@@ -0,0 +1,19 @@
+package com.example.unifastapp.quarter2;
+
+import org.junit.Test;
+
+public class SanAndres_MiniPeta_1 {
+    @Test
+    public void PrintMyProfile() {
+
+        String MyName = "SanAndres";
+        String PetName = "Amari";
+        String FavFood = "Menudo";
+        int MyAge = 16;
+
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + MyName + " And I am " + MyAge + " years old.");
+        System.out.println("I have a wonderful pet named " + PetName + ".");
+        System.out.println("If I could, I would eat " + FavFood + " every single day!");
+    }
+}
