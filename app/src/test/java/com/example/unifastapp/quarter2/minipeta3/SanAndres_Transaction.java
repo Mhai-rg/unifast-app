bf56df128f3ac4cddb6b367fe2601d58779bc8b0
Steve Francis San Andres <sanandresstevefrancis@gmail.com>
2026-09-14 14:27:09 +0800

    SanAndres_Transaction

--- app/src/test/java/com/example/unifastapp/quarter2/minipeta3/SanAndres_Transaction.java
+++ app/src/test/java/com/example/unifastapp/quarter2/minipeta3/SanAndres_Transaction.java
@@ -1,4 +1,24 @@
 package com.example.unifastapp.quarter2.minipeta3;
 
+import org.junit.Test;
 public class SanAndres_Transaction {
+    @Test
+    public void main() {
+
+        int transactionID = 1001;
+
+        // Stores the transaction amount
+        double transactionAmount = 500.00;
+
+        // Records the transaction date
+        String transactionDate = "July 8, 2026";
+
+        // Shows the transaction status
+        Boolean transactionStatus = true;
+
+        System.out.println("Transaction ID: " + transactionID);
+        System.out.println("Transaction Amount: ₱" + transactionAmount);
+        System.out.println("Transaction Date: " + transactionDate);
+        System.out.println("Transaction Status: " + (transactionStatus ? "Completed" : "Pending"));
+    }
 }
