c3080af5fe65bfa70844b6e0679d6bd4834afea7
Hershey mhay D. Miasco <miamhayie@gmail.com>
2026-09-14 14:21:49 +0800

    Miasco_Login code

--- app/src/test/java/com/example/unifastapp/quarter2/minipeta3/Miasco_Login.java
+++ app/src/test/java/com/example/unifastapp/quarter2/minipeta3/Miasco_Login.java
@@ -1,4 +1,25 @@
 package com.example.unifastapp.quarter2.minipeta3;
 
+import org.junit.Test;
+
 public class Miasco_Login {
-}
+    @Test
+        public void main() {
+
+            boolean IsReadyToLogin = true;
+
+            // Stores username
+            String username = "hersheymhaymiasco";
+
+            // Stores password
+            String password = "N0224";
+
+            if (IsReadyToLogin) {
+                System.out.println("Login Successful");
+                System.out.println("Username: " + username);
+                System.out.println("Password: " + password);
+            } else {
+                System.out.println("Login Failed");
+            }
+        }
+    }
