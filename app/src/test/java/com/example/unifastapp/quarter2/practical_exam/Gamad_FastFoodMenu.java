a3134ca9c7bc8325f2b465f6a67f227f29b792f6
Mhai-rg <22-000083@mcstayuman.edu.ph>
2026-09-23 23:16:35 +0800

    Gamad_FastFoodMenu commit 1

--- app/src/test/java/com/example/unifastapp/quarter2/practical_exam/Gamad_FastFoodMenu.java
+++ app/src/test/java/com/example/unifastapp/quarter2/practical_exam/Gamad_FastFoodMenu.java
@@ -1,4 +1,18 @@
 package com.example.unifastapp.quarter2.practical_exam;
 
+import org.junit.Test;
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
+
 public class Gamad_FastFoodMenu {
+    @Test
+    public void start(Scanner scanner) {
+        //Main loop
+    }
+    private void handleBurgerOrder(Scanner scanner) {
+        // burger cases
+    }
+    private void handleFriesOrder() {
+        // fries cases
+    }
 }
