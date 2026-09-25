60745960b17eec577a0da474c88c7d12e1b08d82
Hershey mhay D. Miasco <miamhayie@gmail.com>
2026-09-25 20:18:00 +0800

    Miasco_LibraryKioskMenu committed 2- while loop

--- app/src/test/java/com/example/unifastapp/quarter2/practical_exam/Miasco_LibraryKioskMenu.java
+++ app/src/test/java/com/example/unifastapp/quarter2/practical_exam/Miasco_LibraryKioskMenu.java
@@ -1,14 +1,27 @@
 package com.example.unifastapp.quarter2.practical_exam;
 
-import static java.nio.file.Files.lines;
-
+import java.io.ByteArrayInputStream;
 import org.junit.Test;
-
 import java.util.Scanner;
 
 public class Miasco_LibraryKioskMenu {
     @Test
-    public void borrowBook() {
+    public void start() {
+
+        // Main menu
+        System.out.println("===== LIBRARY KIOSK =====");
+        System.out.println("1. Borrow Book");
+        System.out.println("2. Return Book");
+        System.out.println("3. View Books");
+        System.out.println("4. Exit");
+
+        // Library cases
+        borrowBook();
+        returnBook();
+        viewBooks();
+    }
+
+    private void borrowBook() {
         // Borrow book cases
         System.out.println("Borrow Book");
     }
