5bccb141d6ca5736f9c96ee5e27469d8c0655b03
Steve Francis San Andres <sanandresstevefrancis@gmail.com>
2026-09-25 23:06:37 +0800

    SanAndres_CinemaTicketingMenu commit 2 - while loop

--- app/src/test/java/com/example/unifastapp/quarter2/practical_exam/SanAndres_CinemaTicketingMenu.java
+++ app/src/test/java/com/example/unifastapp/quarter2/practical_exam/SanAndres_CinemaTicketingMenu.java
@@ -5,17 +5,25 @@ import org.junit.Test;
 import java.util.Scanner;
 
 public class SanAndres_CinemaTicketingMenu {
-    @Test
-    public void start(Scanner scanner) {
-        //Main loop
+        @Test
+        public void start(Scanner scanner) {
+            boolean running = true;
+
+            while (running) {
+                System.out.println("=== CINEMA MENU ===");
+                System.out.println("1. Buy Ticket");
+                System.out.println("2. Buy Snacks");
+                System.out.println("3. Exit");
+                System.out.print("Enter choice: ");
+            }
+        }
+        private void handleBuyTicket(Scanner scanner) {
+            // buyticket case
+        }
+        private void handleBuySnacks(Scanner scanner) {
+            // buysnacks case
+        }
     }
-    private void handleBuyTicket(Scanner scanner) {
-        // buyticket case
-    }
-    private void handleBuySnacks(Scanner scanner) {
-        // buysnacks case
-    }
-}
 
 
 
