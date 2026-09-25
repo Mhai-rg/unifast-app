cc0dd41361c0da6203feb1fbe2bff59662f3eeae
Steve Francis San Andres <sanandresstevefrancis@gmail.com>
2026-09-25 22:34:14 +0800

    SanAndres_CinemaTicketingMenu commit 1

--- app/src/test/java/com/example/unifastapp/quarter2/practical_exam/SanAndres_CinemaTicketingMenu.java
+++ app/src/test/java/com/example/unifastapp/quarter2/practical_exam/SanAndres_CinemaTicketingMenu.java
@@ -1,6 +1,33 @@
 package com.example.unifastapp.quarter2.practical_exam;
 
-public class SanAndres_CinemaTicketingMenu {
+import org.junit.Test;
 
+import java.util.Scanner;
+
+public class SanAndres_CinemaTicketingMenu {
+    @Test
+    public void start(Scanner scanner) {
+        //Main loop
+    }
+    private void handleBuyTicket(Scanner scanner) {
+        // buyticket case
+    }
+    private void handleBuySnacks(Scanner scanner) {
+        // buysnacks case
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
